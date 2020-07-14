4e65a6e27793d36cf2adc443ba2959be6fe182ff
akshaykp007 <akshaykp2661998@gmail.com>
2020-07-14 20:52:50 +0100

    Updates

---
+++ precache-manifest.7a83091e4aab5aa80e41ea29357c61ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954e703b1391fc3ea868de0159c0a908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6363c0be027db6404a0b",
+    "url": "/static/css/main.bb51f07d.chunk.css"
+  },
+  {
+    "revision": "03ebb4581ad10d766c1b",
+    "url": "/static/js/2.9d7e23e2.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.9d7e23e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6363c0be027db6404a0b",
+    "url": "/static/js/main.57940a11.chunk.js"
+  },
+  {
+    "revision": "afef85084f0804340619",
+    "url": "/static/js/runtime-main.2b370e64.js"
+  }
+]);
